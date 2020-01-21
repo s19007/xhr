472519e93b6c0b43bddf588f6619ce20563607d2
shingaki <s19007@std.it-college.ac.jp>
2020-01-21 16:08:34 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const  Greeting = props => (
+  h1{props.type})
+class App extends React.Component {
+    constructor (props) {
+      super(props)
+      this.state = { data: []}
+    }
+    render () {
+      return ()}
+    componentDidMount () {
+      fetch('')
+        .then(res => res.json())
+        .then(console.log)
+        .then
+    }
+
+    render () {
+      return (
+export default App;
